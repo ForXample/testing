51a485d5bdb2fc698df7b78fea80f382c0faaea3
Zixi Liu <59849551+ForXample@users.noreply.github.com>
2020-09-24 13:40:44 -0400

    first attempt
    9.24_sunnyland

---
+++ New Unity Project/Assets/player_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }
+
+
+    }
+}
